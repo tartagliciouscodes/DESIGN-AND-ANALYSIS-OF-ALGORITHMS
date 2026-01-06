33e437c1e0597abf18e7b08e253d4cfab2dd22fb
tartagliciouscodes <cynthia25006@gmail.com>
2026-01-06 21:47:17 +0530

    Add files via upload

---
+++ task3/quicksort.c
@@ -0,0 +1,41 @@
+//ch.sc.u4cse24110
+//quick sort
+#include <stdio.h>
+void swap(int *a, int *b) {
+int temp=*a;
+*a=*b;
+*b=temp;
+}
+
+int partition(int arr[], int low, int high) {
+int pivot=arr[high];
+int i=low-1;
+
+for(int j=low; j<high; j++) {
+if (arr[j]<pivot) {
+i++;
+swap(&arr[i],&arr[j]);
+}
+}
+
+swap(&arr[i+1],&arr[high]);
+return i+1;
+}
+
+void quickSort(int arr[], int low, int high) {
+if (low<high) {
+int pi= partition(arr,low,high);
+quickSort(arr,low,pi-1);
+quickSort(arr,pi+1,high);
+}
+}
+
+int main() {
+int arr[]={157,110,147,122,111,149,151,141,123,112,117,133};
+int n= sizeof(arr)/sizeof(arr[0]);
+quickSort(arr,0,n-1);
+printf("sorted array:");
+for(int i=0;i<n;i++) {
+printf("%d ",arr[i]); }
+return 0;
+}
